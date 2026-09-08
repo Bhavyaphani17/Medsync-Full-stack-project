d7d1990b492fa793b4de8a867991021fe4a721fb
Bhavya Phani Sri Vadlamudi <vadlamudibhavya2004@gmail.com>
2026-09-09 00:43:35 +0530

    Fix Feign client scanning

--- storage-service/storage-service/src/main/java/com/medsync/storage_service/StorageServiceApplication.java
+++ storage-service/storage-service/src/main/java/com/medsync/storage_service/StorageServiceApplication.java
@@ -2,13 +2,10 @@ package com.medsync.storage_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
+@EnableFeignClients(basePackages = "com.medsync.storage_service.feign")
 public class StorageServiceApplication {
 
     public static void main(String[] args) {
