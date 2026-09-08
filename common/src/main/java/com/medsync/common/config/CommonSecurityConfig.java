c8a7b1fdf0838ea38b36560f60542c3742a66e0f
Bhavya Phani Sri Vadlamudi <vadlamudibhavya2004@gmail.com>
2026-09-08 23:48:58 +0530

    Add storage service health endpoint

--- common/src/main/java/com/medsync/common/config/CommonSecurityConfig.java
+++ common/src/main/java/com/medsync/common/config/CommonSecurityConfig.java
@@ -22,13 +22,7 @@ public class CommonSecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/",
-                                "/v3/api-docs/**",
-                                "/swagger-ui/**",
-                                "/swagger-ui.html",
-                                "/actuator/**"
-                        ).permitAll()
+                        .requestMatchers("/", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/actuator/**").permitAll()
                 )
                 .addFilterBefore(gatewayHeaderAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
--- storage-service/storage-service/src/main/java/com/medsync/storage_service/StorageServiceApplication.java
+++ storage-service/storage-service/src/main/java/com/medsync/storage_service/StorageServiceApplication.java
@@ -1,14 +1,21 @@
 package com.medsync.storage_service;
 
+import com.medsync.common.security.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {
-        "com.medsync.storage_service",
-        "com.medsync.common"
-})
+@SpringBootApplication(
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                DataSourceTransactionManagerAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class
+        }
+)
+@EnableConfigurationProperties(JwtProperties.class)
 public class StorageServiceApplication {
 
     public static void main(String[] args) {
