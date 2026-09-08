038fa51ab74a06eab505f525e7c8d065cd8bee32
Bhavya Phani Sri Vadlamudi <vadlamudibhavya2004@gmail.com>
2026-09-08 23:56:34 +0530

    Fix storage service Feign client configuration

--- storage-service/storage-service/src/main/java/com/medsync/storage_service/StorageServiceApplication.java
+++ storage-service/storage-service/src/main/java/com/medsync/storage_service/StorageServiceApplication.java
@@ -3,18 +3,16 @@ package com.medsync.storage_service;
 import com.medsync.common.security.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(
-        exclude = {
-                DataSourceAutoConfiguration.class,
-                DataSourceTransactionManagerAutoConfiguration.class,
-                HibernateJpaAutoConfiguration.class
+        scanBasePackages = {
+                "com.medsync.storage_service",
+                "com.medsync.common"
         }
 )
+@EnableFeignClients
 @EnableConfigurationProperties(JwtProperties.class)
 public class StorageServiceApplication {
 
