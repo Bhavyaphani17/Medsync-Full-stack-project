b678eabafbd2112673602a4e6f9a5a02aaa8954f
Bhavya Phani Sri Vadlamudi <vadlamudibhavya2004@gmail.com>
2026-09-08 17:56:03 +0530

    Add storage service health endpoint

---
+++ storage-service/storage-service/src/main/java/com/medsync/storage_service/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.medsync.storage_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "Storage Service is running";
+    }
+}
